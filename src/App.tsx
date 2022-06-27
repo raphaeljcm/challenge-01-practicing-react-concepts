e2945a1d6087bfe284dbf4885fb9e85d37771e40
Raphael Marques <raphaeljcmarques@gmail.com>
2022-06-26 22:20:37 -0300

    feat: adding drag and drop on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import styles from './App.module.scss';
 import './styles/global.scss';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
+import {isMobile} from 'react-device-detect';
 
 function App() {
   return (
-    <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
       <Header />
 
       <main className={styles.container}>
